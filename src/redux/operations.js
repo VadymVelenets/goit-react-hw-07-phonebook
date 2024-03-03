b83c3a42a024378153df3f18faf2395c803da2b9
VadymVelenets <128249544+VadymVelenets@users.noreply.github.com>
2024-03-03 23:35:31 +0200

    update op.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://65e4c71b3070132b3b2543d0.mockapi.io/';
+axios.defaults.baseURL = 'https://65e4e7d23070132b3b256b37.mockapi.io/';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/FetchAll',
